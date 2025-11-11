916ad94fb7eb621d4baf012ae43c3561c5e4a14b
Jing Cao <jcao0715@gmail.com>
2025-11-11 09:35:38 +0000

    integate action chunk instead of one action at a time

--- src/inference/droid_jointpos.py
+++ src/inference/droid_jointpos.py
@@ -21,7 +21,6 @@ class Client(InferenceClient):
         
         self.actions_from_chunk_completed = 0
         self.pred_action_chunk = None
-        self.is_first_inference = True
 
     def _annotate_images(self, exterior_img, wrist_img):
         """
@@ -30,35 +29,7 @@ class Client(InferenceClient):
         """
         exterior_annotated = exterior_img.copy()
         wrist_annotated = wrist_img.copy()
-        h, w = exterior_annotated.shape[:2]
 
-        # exterior annotations
-        # can
-        can_center = (int(9/16*w)+3, int(9/16*h)-5)
-        cv2.circle(exterior_annotated, can_center, 7, (255, 0, 0), 2)
-        can_text = (int(8/16*w), int(10/16*h)+5)
-        # cv2.putText(exterior_annotated, "can", can_text, cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 0, 0), 2)
-        cv2.putText(exterior_annotated, "1", can_center, cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255), 2)
-        # mug
-        mug_center = (int(10/16*w), int(8/16*h)+2)
-        cv2.circle(exterior_annotated, mug_center, 7, (0, 0, 255), 2)
-        mug_text = (int(11/16*w)-6, int(8/16*h)+2)
-        # cv2.putText(exterior_annotated, "mug", mug_text, cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
-        cv2.putText(exterior_annotated, "2", mug_center, cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255), 2)
-
-        # wrist annotations
-        # can
-        can_center = (int(10/16*w), int(9/16*h)-6)
-        cv2.circle(wrist_annotated, can_center, 16, (255, 0, 0), 2)
-        can_text = (int(10/16*w), int(7/16*h))
-        # cv2.putText(wrist_annotated, "can", can_text, cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 0, 0), 2)
-        cv2.putText(wrist_annotated, "1", can_center, cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255), 2)
-        # mug
-        mug_center = (int(7/16*w)-2, int(9/16*h)-4)
-        cv2.circle(wrist_annotated, mug_center, 20, (0, 0, 255), 2)
-        mug_text = (int(5/16*w), int(7/16*h)-5)
-        # cv2.putText(wrist_annotated, "mug", mug_text, cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
-        cv2.putText(wrist_annotated, "2", mug_center, cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255), 2)
         return exterior_annotated, wrist_annotated
 
     def visualize(self, request: dict):
@@ -88,37 +59,25 @@ class Client(InferenceClient):
 
             exterior_img = image_tools.resize_with_pad(curr_obs["right_image"], 224, 224)
             wrist_img = image_tools.resize_with_pad(curr_obs["wrist_image"], 224, 224)
-            
-            if self.is_first_inference:
-                exterior_img, wrist_img = self._annotate_images(exterior_img, wrist_img)
-                os.makedirs("test_imgs", exist_ok=True)
-                cv2.imwrite("test_imgs/exterior_img.png", cv2.cvtColor(exterior_img, cv2.COLOR_RGB2BGR))
-                cv2.imwrite("test_imgs/wrist_img.png", cv2.cvtColor(wrist_img, cv2.COLOR_RGB2BGR))
-                self.is_first_inference = False
 
             request_data = {
-                # "observation/exterior_image_1_left": image_tools.resize_with_pad(
-                #     curr_obs["right_image"], 224, 224
-                # ),
-                # "observation/wrist_image_left": image_tools.resize_with_pad(
-                #     curr_obs["wrist_image"], 224, 224
-                # ),
                 "observation/exterior_image_1_left": exterior_img,
                 "observation/wrist_image_left": wrist_img,
                 "observation/joint_position": curr_obs["joint_position"],
                 "observation/gripper_position": curr_obs["gripper_position"],
                 "prompt": instruction,
             }
-            self.pred_action_chunk = self.client.infer(request_data)["actions"]
+            pred_chunk = self.client.infer(request_data)["actions"] # velocities
+            pred_chunk = pred_chunk.copy()
+            # convert velocities to joint positions
+            dt = 1.0/15.0
+            pred_chunk[:, :-1] *= dt
+            pred_chunk[:, :-1] = np.cumsum(pred_chunk[:, :-1], axis=0)
+            pred_chunk[:, :-1] += curr_obs["joint_position"]
+            self.pred_action_chunk = pred_chunk
 
         action = self.pred_action_chunk[self.actions_from_chunk_completed]
         self.actions_from_chunk_completed += 1
-
-        # NEW EDITS #
-        dt = 1.0/15.0
-        action = action.copy()
-        action[:-1] = curr_obs["joint_position"] + action[:-1] * dt
-        # END NEW EDITS #
 
         # binarize gripper action
         if action[-1].item() > 0.5:
