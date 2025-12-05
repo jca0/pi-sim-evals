7e0ff24b339a2fe39fb8b33471018a7fdd101ce4
Jing Cao <jcao0715@gmail.com>
2025-12-05 09:13:26 +0000

    play cutamp trajecotyr

---
+++ pkl_eval.py
@@ -0,0 +1,148 @@
+"""
+Example script for running 10 rollouts of a DROID policy on the example environment.
+
+Usage:
+
+First, make sure you download the simulation assets and unpack them into the root directory of this package.
+
+Then, in a separate terminal, launch the policy server on localhost:8000 
+-- make sure to set XLA_PYTHON_CLIENT_MEM_FRACTION to avoid JAX hogging all the GPU memory.
+
+For example, to launch a pi0-FAST-DROID policy (with joint position control), 
+run the command below in a separate terminal from the openpi "karl/droid_policies" branch:
+
+XLA_PYTHON_CLIENT_MEM_FRACTION=0.5 uv run scripts/serve_policy.py policy:checkpoint --policy.config=pi0_fast_droid_jointpos --policy.dir=s3://openpi-assets-simeval/pi0_fast_droid_jointpos
+
+Finally, run the evaluation script:
+
+python run_eval.py --episodes 10 --headless
+"""
+
+import tyro
+import argparse
+import gymnasium as gym
+import torch
+import cv2
+import mediapy
+from datetime import datetime
+from pathlib import Path
+from tqdm import tqdm
+from typing import Literal
+import pickle
+import sys
+import os
+
+from src.inference.cutamp_jointpos import Client as CutampJointPosClient
+
+CUROBO_PATH = "/home/ubuntu/curobo/src"
+if CUROBO_PATH not in sys.path:
+    sys.path.insert(0, CUROBO_PATH)
+
+def main(
+        episodes:int = 10,
+        headless: bool = True,
+        scene: int = 1,
+        policy: Literal["pi0.5", "pi0"] = "pi0.5",
+        ):
+    # launch omniverse app with arguments (inside function to prevent overriding tyro)
+    from isaaclab.app import AppLauncher
+    parser = argparse.ArgumentParser(description="Tutorial on creating an empty stage.")
+    AppLauncher.add_app_launcher_args(parser)
+    args_cli, _ = parser.parse_known_args()
+    args_cli.enable_cameras = True
+    args_cli.headless = headless
+    args_cli.policy = policy
+    app_launcher = AppLauncher(args_cli)
+    simulation_app = app_launcher.app
+
+    # All IsaacLab dependent modules should be imported after the app is launched
+    import src.environments # noqa: F401
+    from isaaclab_tasks.utils import parse_env_cfg
+
+
+    # Initialize the env
+    env_cfg = parse_env_cfg(
+        "DROID",
+        device=args_cli.device,
+        num_envs=1,
+        use_fabric=True,
+    )
+    instruction = None
+    match scene:
+        case 1:
+            instruction = "put the cube in the bowl"
+        case 2:
+            instruction = "put the can in the mug"
+        case 3:
+            instruction = "put banana in the bin"
+        case 4: 
+            instruction = "put the meat can on the sugar box"
+        case 5:
+            instruction = "rearrange the cubes so that they spell 'REX'"
+        case 6:
+            instruction = "stack all the cubes on top of each other"
+        case _:
+            raise ValueError(f"Scene {scene} not supported")
+        
+    env_cfg.set_scene(scene)
+    env_cfg.episode_length_s = 30.0 # LENGTH OF EPISODE
+    env = gym.make("DROID", cfg=env_cfg)
+
+    obs, _ = env.reset()
+    obs, _ = env.reset() # need second render cycle to get correctly loaded materials
+
+    client = CutampJointPosClient(policy=policy)
+
+    video_dir = Path("runs") / datetime.now().strftime("%Y-%m-%d") / datetime.now().strftime("%H-%M-%S")
+    video_dir.mkdir(parents=True, exist_ok=True)
+    video = []
+    right_video = []
+    wrist_video = []
+    ep = 0
+    max_steps = env.env.max_episode_length
+    with torch.no_grad():
+        for ep in range(episodes):
+            task_completed = False
+            for i in tqdm(range(max_steps), desc=f"Episode {ep+1}/{episodes}"):
+                ret = client.infer(obs, instruction)
+                if not headless:
+                    cv2.imshow("Right Camera", cv2.cvtColor(ret["viz"], cv2.COLOR_RGB2BGR))
+                    cv2.waitKey(1)
+                video.append(ret["viz"])
+                right_video.append(ret["right_image"])
+                wrist_video.append(ret["wrist_image"])
+                action = torch.tensor(ret["action"])[None]
+                obs, _, term, trunc, _ = env.step(action)
+
+                if term or trunc:
+                    break
+
+            client.reset()
+            mediapy.write_video(
+                video_dir / f"{policy}_scene{scene}_ep{ep}.mp4",
+                video,
+                fps=15,
+            )
+            # added right and wrist videos
+            mediapy.write_video(
+                video_dir / f"{policy}_scene{scene}_ep{ep}_right.mp4",
+                right_video,
+                fps=15,
+            )
+            mediapy.write_video(
+                video_dir / f"{policy}_scene{scene}_ep{ep}_wrist.mp4",
+                wrist_video,
+                fps=15,
+            )
+            video = []
+            # reset right and wrist videos
+            right_video = []
+            wrist_video = []
+
+    env.close()
+    simulation_app.close()
+
+if __name__ == "__main__":
+    args = tyro.cli(main)
+
+    
---
+++ src/inference/cutamp_jointpos.py
@@ -0,0 +1,115 @@
+import tyro
+import numpy as np
+from PIL import Image
+from openpi_client import websocket_client_policy, image_tools
+import os
+import cv2
+from .abstract_client import InferenceClient
+from typing import Literal
+
+class Client(InferenceClient):
+    def __init__(self, 
+                remote_host:str = "localhost", 
+                remote_port:int = 8000,
+                open_loop_horizon:int = 8,
+                policy: Literal["pi0.5", "pi0"] = "pi0.5" ) -> None:
+        self.policy = policy
+        if self.policy == "pi0.5":
+            self.open_loop_horizon = 15
+        elif self.policy == "pi0":
+            self.open_loop_horizon = 8
+
+        self.client = websocket_client_policy.WebsocketClientPolicy(
+            remote_host, remote_port
+        )
+        
+        self.actions_from_chunk_completed = 0
+        self.pred_action_chunk = None
+        self.inference_count = 0
+
+    def visualize(self, request: dict):
+        """
+        Return the camera views how the model sees it
+        """
+        curr_obs = self._extract_observation(request)
+        base_img = image_tools.resize_with_pad(curr_obs["right_image"], 224, 224)
+        wrist_img = image_tools.resize_with_pad(curr_obs["wrist_image"], 224, 224)
+        combined = np.concatenate([base_img, wrist_img], axis=1)
+        return combined
+
+    def reset(self):
+        self.actions_from_chunk_completed = 0
+        self.pred_action_chunk = None
+
+    def infer(self, obs: dict, instruction: str) -> dict:
+        """
+        Infer the next action from the policy in a server-client setup
+        """
+        curr_obs = self._extract_observation(obs)
+        if (
+            self.actions_from_chunk_completed == 0
+            or self.actions_from_chunk_completed >= self.open_loop_horizon
+        ):
+            self.actions_from_chunk_completed = 0
+
+            exterior_annotated_resized = image_tools.resize_with_pad(curr_obs["right_image"], 224, 224)
+            wrist_annotated_resized = image_tools.resize_with_pad(curr_obs["wrist_image"], 224, 224)
+            
+            request_data = {
+                "observation/exterior_image_1_left": exterior_annotated_resized,
+                "observation/wrist_image_left": wrist_annotated_resized,
+                "observation/joint_position": curr_obs["joint_position"],
+                "observation/gripper_position": curr_obs["gripper_position"],
+                "prompt": instruction,
+            }
+            pred_chunk = self.client.infer(request_data)["actions"] # velocities
+
+            if self.policy == "pi0.5":
+                # convert velocities to joint positions
+                dt = 1.0/15.0
+                pred_chunk = pred_chunk.copy()
+                pred_chunk[:, :-1] *= dt
+                pred_chunk[:, :-1] = np.cumsum(pred_chunk[:, :-1], axis=0)
+                pred_chunk[:, :-1] += curr_obs["joint_position"]
+            
+            self.pred_action_chunk = pred_chunk
+
+            self.inference_count += 1
+
+        action = self.pred_action_chunk[self.actions_from_chunk_completed]
+        self.actions_from_chunk_completed += 1
+
+        # binarize gripper action
+        if action[-1].item() > 0.5:
+            action = np.concatenate([action[:-1], np.ones((1,))])
+        else:
+            action = np.concatenate([action[:-1], np.zeros((1,))])
+
+        img1 = image_tools.resize_with_pad(curr_obs["right_image"], 224, 224)
+        img2 = image_tools.resize_with_pad(curr_obs["wrist_image"], 224, 224)
+        both = np.concatenate([img1, img2], axis=1)
+
+        # added right image and wrist image to dict
+        return {"action": action, "viz": both, "right_image": curr_obs["right_image"], "wrist_image": curr_obs["wrist_image"]}
+
+    def _extract_observation(self, obs_dict, *, save_to_disk=False):
+        # Assign images
+        right_image = obs_dict["policy"]["external_cam"][0].clone().detach().cpu().numpy()
+        wrist_image = obs_dict["policy"]["wrist_cam"][0].clone().detach().cpu().numpy()
+
+        # Capture proprioceptive state
+        robot_state = obs_dict["policy"]
+        joint_position = robot_state["arm_joint_pos"].clone().detach().cpu().numpy()
+        gripper_position = robot_state["gripper_pos"].clone().detach().cpu().numpy()
+
+        if save_to_disk:
+            combined_image = np.concatenate([right_image, wrist_image], axis=1)
+            combined_image = Image.fromarray(combined_image)
+            combined_image.save("robot_camera_views.png")
+
+        return {
+            "right_image": right_image,
+            "wrist_image": wrist_image,
+            "joint_position": joint_position,
+            "gripper_position": gripper_position,
+        }
